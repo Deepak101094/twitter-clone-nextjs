6ae6ab04758b89136faac90db326ac1ce3f129bb
Deepak/101094 <deepakpandey101094@gmail.com>
2024-03-21 00:17:47 +0530

    create sidebar

---
+++ components/layout/FollowBar.tsx
@@ -0,0 +1,10 @@
+function FollowBar() {
+	return (
+		<div className='px-6 py-4 hidden lg:block'>
+			<div className='bg-netural-800 rounded-xl p-4'>
+				<h3 className='text-xl text-white font-semibold'> Who to follow</h3>
+			</div>
+		</div>
+	);
+}
+export default FollowBar;
